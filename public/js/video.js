a805f4072461c21e8b9c933b4e04aa6d8681caf7
Vitor <vitoraot@gmail.com>
2020-09-14 22:34:43 -0300

    Commit inicial

--- public/js/video.js
+++
@@ -1,9 +0,0 @@
-video = document.getElementById('video');
-url = document.getElementById('video_url');
-
-url.addEventListener('keyup', function () {//registo  é feito com addEventListener
-    video.disabled = url.value.trim().length > 0;
-});
-function desativaURL (){
-    url.disabled = "disable";
-};
